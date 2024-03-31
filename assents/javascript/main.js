490f724d8966437743335ea857d9fca3fbeb373b
Lucas Wagner de Melo Fogaça <141058064+LucasWMF@users.noreply.github.com>
2024-03-30 23:07:32 -0300

    JS configs não terminado

--- assents/javascript/main.js
+++ assents/javascript/main.js
@@ -1,64 +1,127 @@
-// console.log('Teste')
 
-// $(document).ready(function() {
-//     $('#mobile_btn').on('click', function () {
-//         $('#mobile_menu').toggleClass('active');
-//         $('#mobile_btn').find('i').toggleClass('fa-x');
-//     });
+let currentPage = window.location.pathname.split('/').pop(); // Obtém o nome da página atual
 
-//     const sections = $('section');
-//     const navItems = $('.nav-item');
+if (currentPage === 'test.html') {
+    PasswordChecked();
+    registred();
+} else if (currentPage === 'database.html') {
+    dataBaseInfo();
+    registred();
+}
 
-//     $(window).on('scroll', function () {
-//         const header = $('.main-header');
-//         const scrollPosition = $(window).scrollTop() - header.outerHeight();
 
-//         let activeSectionIndex = 0;
+// User Registration
+// Registro de Usuário
 
-//         if (scrollPosition <= 0) {
-//             header.css('box-shadow', 'none');
-//         } else {
-//             header.css('box-shadow', '0px 100px 100px -7px black;');
-//         }
+function PasswordChecked() {
+    document.getElementById("registrationForm").addEventListener("submit", function (event) {
+        event.preventDefault();
 
-//         sections.each(function(i) {
-//             const section = $(this);
-//             const sectionTop = section.offset().top - 96;
-//             const sectionBottom = sectionTop + section.outerHeight();
+        let password = document.getElementById('user-password').value;
+        let checkPassword = document.getElementById('check-password').value;
 
-//             if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-//                 activeSectionIndex = i;
-//                 return false;
-//             }
-//         });
+        if (checkPassword != password) {
+            alert(`As senhas não coincidem. Tente Novamente`);
+        } else {
+            let userName = document.getElementById("user-name").value;
+            alert(`Registro bem-sucedido para o usuário ${userName}.`);
 
-//         navItems.removeClass('active');
-//         $(navItems[activeSectionIndex]).addClass('active');
-//     });
+            registred();
+            document.getElementById('registrationForm').reset();
+        }
+    });
+}
 
-//     ScrollReveal().reveal('#home', {
-//         origin: 'left',
-//         duration: 2000,
-//         distance: '20%'
-//     });
-//     ScrollReveal().reveal('#about', {
-//         origin: 'left',
-//         duration: 2000,
-//         distance: '20%'
-//     });
-//     ScrollReveal().reveal('#menu', {
-//         origin: 'left',
-//         duration: 2000,
-//         distance: '20%'
-//     });
-//     ScrollReveal().reveal('#testimonials', {
-//         origin: 'left',
-//         duration: 2000,
-//         distance: '20%'
-//     });
-//     ScrollReveal().reveal('#location', {
-//         origin: 'left',
-//         duration: 2000,
-//         distance: '20%'
-//     });
-// });
+function registred() {
+    // for (let i = 0; i < localStorage.length; i++) {
+    //     let key = localStorage.key(i);
+    //     let value = localStorage.getItem(key);
+    //     console.log(`Chave: ${key}, Valor: ${value}`);
+    // }
+
+    let id;
+    let nameUser = document.getElementById('user-name').textContent;
+    let emailUser = document.getElementById('user-email').textContent;
+    let passwordUser = document.getElementById('user-password').textContent;
+
+    //   if (nameUser == null && emailUser == null && passwordUser == null) {
+      //      alert('Campos não digitados!');
+
+    //} //else {
+       // alert('Entrei')
+
+        let number = localStorage.getItem('userCounting', number);
+        let idData = [];
+
+        number++;
+        alert('ConteiEntrei')
+    }
+    localStorage.setItem('userCounting', number);
+
+    if (number > 10) {
+        id = number.toString().padStart(8, '0');
+    } else if (number < 10) {
+        id = number.toString().padStart(7, '0');
+    } else if (number >= 100) {
+        id = number.toString().padStart(6, '0');
+    } else if (number >= 1000) {
+        id = number.toString().padStart(5, '0');
+    } else {
+        id = number.toString().padStart(4, '0');
+    }
+    idData.push(id);
+
+    let idName = `${id}/registration/name`;
+    let idEmail = `${id}/registration/email`;
+    let idPassword = `${id}/registration/password`;
+
+    localStorage.setItem(idName, nameUser);
+    localStorage.setItem(idEmail, emailUser);
+    localStorage.setItem(idPassword, passwordUser);
+
+    for (let i = 0; i < idData.length; i++) {
+        console.log(idData[i]);
+    }
+    console.log(localStorage.getItem('userCounting'));
+    return idData;
+}
+
+// User Profile
+// Perfil do Usuário
+function profileDisplay(userConnect) {
+    let profileButton = document.getElementsById('user-access');
+    profileDisplay();
+    profileButton.onclick = function () {
+        if (userConnect) {
+            alert('Test True');
+            window.location.href = 'profile.html';
+        } else {
+            alert('Test False');
+            loginscreen.showModal();
+        }
+    }
+}
+
+// Database Display
+// Exibição do Banco de Dados
+// function dataBaseInfo(idData) {
+//     let container = document.getElementById('containerTable');
+//     let tableHTML = `<table class="table-dataBase">
+//                         <h1 class="title-dataBase">Dados dos Usuários</h1>
+//                         <tr>
+//                             <th>ID</th>
+//                             <th>Nome de Usuário</th>
+//                             <th>Email</th>
+//                         </tr>`;
+
+//     for (let i = 0; i < idData.length; i++) {
+//         let id = idData[i];
+//         tableHTML += `<tr>
+//         <td>${localStorage.getItem(id)}</td>
+//         <td>${localStorage.getItem(id + '/registration/name')}</td>
+//         <td>${localStorage.getItem(id + '/registration/email')}</td>
+//     </tr>`;
+
+//     tableHTML += `</table/>`;
+//     container.innerHTML = tableHTML;
+// }
