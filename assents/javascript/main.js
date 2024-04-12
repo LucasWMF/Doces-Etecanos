9b7a7ce582c452c27f4cc68a4fa59384f9b8afd6
Lucas Wagner de Melo Fogaça <141058064+LucasWMF@users.noreply.github.com>
2024-04-12 06:21:50 -0300

    Lucas

--- assents/javascript/main.js
+++ assents/javascript/main.js
@@ -1,31 +1,21 @@
-// EventListener's Front
 
-document.addEventListener("mousemove", function (event) {
-    let mouseY = event.clientY;
-    let header = document.querySelector('.main-header');
-
-    if (mouseY > (window.innerHeight - 500)) {
-        // Se o cursor estiver abaixo de (altura da janela - 500)
-        console.log("O cursor se moveu para baixo.");
-
-        // Adiciona a classe 'sticky'
-        header.classList.add('sticky');
-    } else {
-        // Se o cursor estiver acima de (altura da janela - 500)
-        // Remove a classe 'sticky'
-        header.classList.remove('sticky');
-    }
-});
-
-
+// ---------------------- Back Code ---------------------- //
 // EventListener's Back
 
 window.addEventListener("beforeunload", function (event) {
     if (event.target === window) {
         if (connected === true) {
-            alert(`Sua conta foi reiniciada`);
+            alert(`Sua conta foi desconectada`);
         }
     }
+});
+
+// Adiciona um evento de clique ao botão de hamburguer
+document.querySelector('#mobile_menu button').addEventListener('click', function() {
+    // Obtém o elemento do menu móvel
+    let mobileMenu = document.querySelector('#mobile_menu');
+    // Alterna a classe 'active' no menu móvel
+    mobileMenu.classList.toggle('mobileon');
 });
 
 document.addEventListener("submit", function (event) {
@@ -40,40 +30,13 @@ document.addEventListener("submit", function (event) {
     }
 });
 
-
-document.getElementById('user-access').addEventListener("click", () => {
-    abrirModal();
-});
-
-document.getElementById('exit-modal').addEventListener("click", () => {
-    fecharModal();
-});
-
-document.getElementsByClassName('container-modal').addEventListener("click", () => {
-    fecharModal();
-});
-
-
-// Variáveis Function
+// Variable Functions
+// Variáveis das Funções
 
 let number;
 let idData = [];
 let connected = false;
 number = localStorage.getItem('numberCounting');
-
-// Functions Front
-
-function abrirModal() {
-    let modal = document.getElementById('modal-login')
-    modal.classList.add('active')
-    modal.classList.remove('notactive')
-}
-
-function fecharModal() {
-    let modal = document.getElementById('modal - login')
-    modal.classList.remove('active')
-    modal.classList.add('notactive')
-}
 
 // Functions Back
 
@@ -180,3 +143,109 @@ function dataBase() {
 
     tableHTML.innerHTML += `</table>`;
 }
+
+// ---------------------- Front Code ---------------------- //
+// EventListener's Front
+
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('.main-header');
+
+    if (window.scrollY > 100) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    let passwordEye = document.getElementById('password-eye');
+    let inputPassword = document.getElementById('password-login')
+
+    passwordEye.addEventListener('click', function() {
+        if (passwordEye.classList.contains('fa-eye')) {
+            passwordEye.classList.remove('fa-eye');
+            passwordEye.classList.add('fa-eye-slash');
+            inputPassword.type = 'text'
+        } else {
+            passwordEye.classList.remove('fa-eye-slash');
+            passwordEye.classList.add('fa-eye');
+            inputPassword.type = 'password'
+        }
+    });
+});
+
+// Modal Screens
+// Telas Modais
+
+// Selecionando os elementos
+// o id do botão que fica no usuário
+// Criar condição que se não tiver logado abre a tela de login ao envez da tela do Usuário.
+// O id do botão que fica para criar avaliações
+// Criar condição que o openModalReview abre o Login se não tiver conectado e se tiver abre para criar avaliação.
+
+// let btnUser = document.getElementById('user-access');
+// // let btnReview = document.getElementById('create-review');
+// let closeModal = document.getElementById('exit-modal');
+// let fade = document.getElementsByClassName('fade');
+
+// // Adicionando os ouvintes de eventos para abrir modais
+// btnUser.addEventListener("click", () => toggleModal('user'));
+// // btnReview.addEventListener("click", () => toggleModal('review'));
+
+// // Adicionando os ouvintes de eventos para fechar modais
+// closeModal.addEventListener("click", () => toggleModal('close'));
+// fade.addEventListener("click", () => toggleModal('close'));
+
+function toggleModal() {
+    let modal = document.getElementById("modal-login");
+    let fade = document.querySelector(".fade");
+    let container = document.querySelector('.container-modal');
+
+    [modal, fade, container].forEach((el) => el.classList.toggle("active"));
+}
+
+function setupModal() {
+    let btnUser = document.getElementById("user-access");
+    let closeModal = document.getElementById("exit-modal");
+    let fade = document.querySelector(".fade");
+    let container = document.querySelector('.container-modal');
+
+    [btnUser, closeModal, fade].forEach((el) => {
+        el.addEventListener("click", toggleModal);
+    });
+}
+setupModal()
+
+// Functions Modal
+// Funções para os Modais
+
+// Função para definir o modal com base no tipo
+// function modalDefined(modalType) {
+//     let modal;
+//     if (modalType === 'user') {
+//         if (connected === true) {
+//             modal = document.getElementById('modal-user');
+//         } else if (userConnected === userAdmin) {
+//             modal = document.getElementById('modal-database');
+//         } else {
+//             modal = document.getElementById('modal-login');
+//         }
+//     } else {
+//         if (connected === true) {
+//             modal = document.getElementById('modal-reviews');
+//         } else {
+//             modal = document.getElementById('modal-login');
+//         }
+//     }
+//     return modal;
+// }
+
+// Função para alternar o modal e o fade (fundo escuro)
+// function toggleModal(modalType) {
+//     // let modal = modalDefined(modalType);
+//     let modal = document.getElementById('modal-login')
+//     if (modal) {
+//         modal.classList.toggle('active');
+//         fade.classList.toggle('active');
+//     }
+// }
